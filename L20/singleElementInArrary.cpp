8ccd55aecd072cac23a82fcad158ff67e0c57de8
25f2001422 <25f2001422@ds.study.iitm.ac.in>
2026-01-25 19:18:19 +0530

    MInor changes

--- L20/singleElementInArrary.cpp
+++ L20/singleElementInArrary.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int single_element(vector<int>& vec);
 int main()
 {
-    vector <int> arr={1,1,2,2,3,3,4,4,5,6,6,7,7};
+    vector <int> arr={1,2,2,3,3,4,4,5,5,6,6,7,7};
 
     cout<<single_element(arr)<<endl;
 }
@@ -15,10 +15,15 @@ int single_element(vector<int>&vec)
     int st=0;
     int end=vec.size()-1;
 
+    if(end==1) return vec[end];
+
     while(st<=end)
     {
+
         int mid=st+(end-st)/2;
 
+        if(mid==0 && vec[mid]!=vec[mid+1]) return vec[mid];
+        if(mid==end-1 && vec[mid]!=vec[end-2]) return vec[mid];
         if(vec[mid-1]!=vec[mid] && vec[mid]!=vec[mid+1])
         {
             return vec[mid];

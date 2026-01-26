29bae1a8812bdee03a3c3308ba837581b2952130
25f2001422 <25f2001422@ds.study.iitm.ac.in>
2026-01-26 16:09:53 +0530

    L21

---
+++ L21/BookAllocationProblem.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int allocateBook(vector<int>&arr,int n,int m);
+int checkValidity(vector<int> arr,int n,int m,int mid);
+int main()
+{
+    vector<int> arr={15,17,20};
+    int n=4, m=2;
+
+    cout<<allocateBook(arr,n,m)<<endl;
+
+    return 0;
+}
+
+int allocateBook(vector<int>&arr,int n,int m)
+{
+    int ans=-1;
+    int end=0;
+    if(m>n)
+    {
+        return ans;
+    }
+    for(int i:arr)
+    {
+        end+=i;
+    }
+
+    
+    int st=0;
+    while(st<end)
+    {
+        int mid=st+(end-st)/2;
+
+        if(checkValidity(arr,n,m,mid))
+        {
+            end=mid-1;
+            ans=mid;
+        }
+        else
+        {
+            st=mid+1;
+        }
+    }
+    return ans;
+}
+
+int checkValidity(vector<int> arr,int n,int m,int mid)
+{
+    int stu=1;
+    int page=0;
+    for(int i:arr)
+    {
+        if(i>mid)
+        {
+            return false;
+        }
+        if(page+i<=mid)
+        {
+            page+=i;
+        }
+        else
+        {
+            stu++;
+            page=i;
+        }
+    }
+
+    return stu>m ? false : true;
+
+}
